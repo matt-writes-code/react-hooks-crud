4c278829e89c4b4efe0a5fe8272d3646085ececa
matt-writes-code <37174946+mayojich@users.noreply.github.com>
2019-02-08 10:53:08 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserTable from './tables/UserTable';
+import AddUserForm from './forms/AddUserForm';
+import EditUserForm from './forms/EditUserForm';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const usersData = [
+    { id: 1, name: "Tyler", username: 'Tyler_Knight95' },
+    { id: 2, name: "Braddy", username: "Braddy_Pomp91" },
+    { id: 3, name: "Clay", username: "Clay_Stone88" }
+  ];
+
+  const [users, setUsers] = useState(usersData);
+
+  const addUser = user => {
+    user.id = users.length + 1
+    setUsers([...users, user])
   }
+
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id))
+    setEditing(false)
+  }
+
+  const [editing, setEditing] = useState(false)
+
+  const initialFormState = { id: null, name: "", username: "" }
+
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+
+  const editRow = user => {
+    setEditing(true)
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+  }
+
+  return (
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          {editing ?
+            (<div>
+              <h2>Edit user</h2>
+              <EditUserForm
+                editing={editing}
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser} />
+            </div>) : (
+              <div>
+                <h2>Add user</h2>
+                <AddUserForm addUser={addUser} />
+              </div>
+            )}
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+        </div>
+      </div>
+      <a href="https://www.taniarascia.com/crud-app-in-react-with-hooks/" target="_blank">Tutorial</a>
+    </div>
+  )
 }
 
 export default App;
